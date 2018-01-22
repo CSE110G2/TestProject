cf6d4cfa30cad9e0e401e72747a028e96ee660fa
Limuzhou <mul023@ucsd.edu>
2018-01-22 11:41:42 -0800

    5

--- app/src/main/java/com/android/testproject/MainActivity.java
+++ app/src/main/java/com/android/testproject/MainActivity.java
@@ -8,7 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-//some changes 1
+//some changes 2
 public class MainActivity extends AppCompatActivity {
 
     @Override
